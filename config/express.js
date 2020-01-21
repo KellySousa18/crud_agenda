b3d8b14fdb3129c5e0a3d8c86ccbdac95af9b9e6
KELLY SOUSA <KELLY.SOUSA@ACAD.IFMA.EDU.BR>
2020-01-21 16:22:46 -0300

    add express

---
+++ config/express.js
@@ -0,0 +1,25 @@
+
+let express = require('express');
+let consign = require('consign');  
+let bodyParser = require('body-parser');
+let expressValidator = require('express-validator');
+
+let app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true} ));
+app.use(expressValidator() );
+
+consign({cwd:'app'})
+   .include('rotas') 
+   .then('infra')
+   .then('repositorio')
+   .into(app);
+
+module.exports = function() {
+    return app;
+}
+
+ 
